75e117175eaa659917cc81cdf2bcfb83d10677f5
Lisa <1144030197@qq.com>
2016-07-17 22:27:50 +0800

    完成了输赢判断

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-
+var boxsize = 15;
 window.onload = function() {
-	var boxsize = 15;
+	//初始化棋盘
 	var box = document.getElementById('box');
 	for (var i = 0; i < boxsize; ++i) {
 		var row = document.createElement('tr');
@@ -13,115 +13,78 @@ window.onload = function() {
 		box.appendChild(row);
 	}
 }
-function getCellValue(x, y) {
-	var cell = document.getElementById(y+"-"+x).firstChild;
-	console.log(cell);
-	return cell ? cell.className : '';
-}
 
 var currentStep = 0;
-function handleCellClick(cell){
+function handleCellClick(cell){ 
 	var idValue = cell.getAttribute("id");
 	var x = parseInt(idValue.split("-")[1]);
     var y = parseInt(idValue.split("-")[0]);
 	
 	if (!cell.hasChildNodes()){
-		console.log("x=" + x);
-		console.log("y=" + y);
-		console.log("this color is " + role.innerHTML);
-		var blackCount = 1;
-		var whiteCount = 1;
-		
-		for(var i = x-1;;--i){
-			if(getCellValue(i, y) == 'white'){
-				++whiteCount;
-			}else{
-				break;
-			}
-		}
-		for(var i = x+1;;++i){
-			if(getCellValue(i, y) == 'white'){
-				++whiteCount;
-			}else{
-				break;
-			}					
-		}			
-		
-		for(var i = x-1;;--i){
-			if(getCellValue(i, y) == 'black'){
-				++blackCount;
-			}else{
-				break;
-			}
-		}
-		for(var i = x+1;;++i){
-			if(getCellValue(i, y) == 'black'){
-				++blackCount;
-			}else{
-				break;
-			}					
-		}		
-				
-		for(var j = y-1;;--j){
-			if(getCellValue(x,j) == 'black'){
-				++blackCount;
-			}else{
-				break;
-			}
-		}
-		for(var j = y+1;;++j){
-			if(getCellValue(x,j) == 'black'){
-				++blackCount;
-			}else{
-				break;
-			}
-		}
-		
-		for(var j = y-1;;--j){
-			if(getCellValue(x,j) == 'white'){
-				++whiteCount;
-			}else{
-				break;
-			}
-		}
-		for(var j = y+1;;++j){
-			if(getCellValue(x,j) == 'white'){
-				++whiteCount;
-			}else{
-				break;
-			}
-		}
-		
-				
-		
-		
-		
-		
+		// 落子行为
+		var previousChessColor;
 		var div = document.createElement('div');
 		if(role.innerHTML == "黑棋"){
 			div.setAttribute('class', 'black');
-			
-			
+			previousChessColor = '黑棋';
 			role.innerHTML = '白棋';
 			//step.innerHTML = parseInt(step.innerHTML) + 1; 
 		}else{
 			div.setAttribute('class','white');
+			previousChessColor = '白棋';
 			role.innerHTML = '黑棋';
 			//step.innerHTML = parseInt(step.innerHTML) + 1;
 		}
 		cell.appendChild(div);
 		step.innerHTML = ++currentStep;
 		
-		if(whiteCount>=5){
-			alert('白棋胜');
-
+		//判断赢
+		var steps = [[1, 0], [0, 1], [1, 1], [1, -1]];
+		for(var i = 0; i < 4; i++) {
+			if(judgeChessWin(x, steps[i][0], y, steps[i][1])){
+				alert(previousChessColor + "赢了");
+				//禁用棋盘
+			}
 		}
-		if(blackCount>=5){
-			alert('黑棋胜');
-
-		}
+		
 	}
 }
+
+//拿到单元格颜色
+function getCellColor(x, y) {
+	if(outOfRange(x, y)) {
+		return '';
+	}
+	var cell = document.getElementById(y+"-"+x).firstChild;
+	return cell ? cell.className : '';
+}
+//判断是否越界
+function outOfRange(x, y) {
+	return x < 0 || x >= boxsize || y < 0 || y >= boxsize;
+}
+//计算落子某个方向上的棋子数量
+function caculateChess(chessColor, x, xstep, y, ystep){
+	var nextX = x + xstep;
+	var nextY = y + ystep;
+	var nextColor = getCellColor(nextX, nextY);
+	var count = 1;
+	while(nextColor == chessColor && count < 5){
+		count++;
+		nextX = nextX + xstep;
+		nextY = nextY + ystep;
+		nextColor = getCellColor(nextX, nextY);
+	}
+	return count;
+}
+//判断cell中某个落子颜色在某个方向上的数量		
+function judgeChessWin(x, xstep, y, ystep){
+	var chessColor = getCellColor(x, y);
+	var count = 0;
+	count += caculateChess(chessColor, x, xstep, y, ystep);
+	count += caculateChess(chessColor, x, -xstep, y, -ystep);
+	return count > 5;
+}	
+
 
 
 
